ba1a01a8dd661db17272b4f66108ed7ef4a79a77
Malcolm Shi <malcolmshi123@gmail.com>
2024-01-13 20:35:47 -0500

    init commit

--- GojoBracken/Plugin.cs
+++ GojoBracken/Plugin.cs
@@ -1,12 +1,30 @@
-﻿using System;
+﻿using BepInEx;
+using HarmonyLib;
+using System;
+using System.CodeDom;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace GojoBracken
 {
-    public class Class1
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class Plugin : BaseUnityPlugin
     {
+        private const string modGUID = "FreakyFriday.GojoBracken";
+        private const string modName = "Gojo Bracken Mod";
+        private const string modVersion = "1.0.0";
+
+        private Harmony harmony;
+
+        void Awake()
+        {
+            harmony = new Harmony(modGUID);
+            harmony.PatchAll();
+            Logger.LogInfo($"The Bracken is now limitless :)");
+        }
+
     }
 }
