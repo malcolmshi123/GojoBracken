aab524bf8718369a9214124f85d9e48537b6b432
Malcolm Shi <malcolmshi123@gmail.com>
2024-01-14 02:06:21 -0500

    finish base code

---
+++ GojoBracken/Patches/EnemyTypes.cs
@@ -0,0 +1,26 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace GojoBracken.Patches
+{
+    [HarmonyPatch]
+    internal class EnemyTypes
+    {
+        [HarmonyPatch(typeof(FlowermanAI), "Start")]
+        [HarmonyPostfix]
+        public static void SummonGojo(FlowermanAI __instance)
+        {
+            __instance.creatureAngerVoice.clip = LC_API.BundleAPI.BundleLoader.GetLoadedAsset<AudioClip>("assets/AngerVoice.mp3");
+            __instance.crackNeckSFX = LC_API.BundleAPI.BundleLoader.GetLoadedAsset<AudioClip>("assets/CrackNeck.mp3");
+            __instance.crackNeckAudio.clip = LC_API.BundleAPI.BundleLoader.GetLoadedAsset<AudioClip>("assets/CrackNeck.mp3");
+            GameObject.Destroy(__instance.gameObject.transform.Find("FlowermanModel").Find("LOD1").gameObject.GetComponent<SkinnedMeshRenderer>());
+            GameObject Gojo = UnityEngine.Object.Instantiate(LC_API.BundleAPI.BundleLoader.GetLoadedAsset<GameObject>("assets/Gojo.prefab"), __instance.gameObject.transform);
+            Gojo.transform.localPosition = new Vector3(0f, 1.5f, 0f);
+        }
+    }
+}
--- GojoBracken/Patches/TerminalPatches.cs
+++ GojoBracken/Patches/TerminalPatches.cs
@@ -4,19 +4,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Video;
 
 namespace GojoBracken.Patches
 {
-    [HarmonyPatch(typeof(Terminal))]
+    [HarmonyPatch]
     internal class TerminalPatches
     {
-        [HarmonyPatch("Awake")]
+        [HarmonyPatch(typeof(Terminal), "Awake")]
         [HarmonyPostfix]
 
         public static void EditTerminal(Terminal __instance)
         {
             __instance.terminalNodes.allKeywords[36].word = "gojo";
             __instance.enemyFiles[1].creatureName = "Gojo";
+            __instance.enemyFiles[1].displayVideo = LC_API.BundleAPI.BundleLoader.GetLoadedAsset<VideoClip>("assets/Gojo.m4v");
+            __instance.enemyFiles[1].displayText = LC_API.BundleAPI.BundleLoader.GetLoadedAsset<TextAsset>("assets/Gojo.txt").text;
         }
     }
 }
